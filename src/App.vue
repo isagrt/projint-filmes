8b23f4400487f424db18e3d73e9834fbe13cf6a8
isagrt <isadoragoedert05@gmail.com>
2022-09-14 09:11:31 -0300

    mds meu senhor, impossivel tankar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
-import { RouterView } from 'vue-router'
-
+import { RouterView } from "vue-router";
+import HeaderComp from "@/components/template/HeaderComp.vue";
+import FooterComp from "@/components/template/FooterComp.vue";
 </script>
 
 <template>
+  <HeaderComp />
   <RouterView />
+  <FooterComp />
 </template>
 
-<style >
-</style>
+<style></style>
--- src/components/template/HeaderComp.vue
+++ src/components/template/HeaderComp.vue
@@ -1,1 +1,18 @@
-<script></script>
+<template>
+  <header>
+    <div class="menu">
+      <span>Home </span>
+      <span>Categorias </span>
+    </div>
+  </header>
+</template>
+
+<style>
+header span {
+  padding-right: 2%;
+}
+header {
+  border-bottom: 1px solid black;
+  padding: 1%;
+}
+</style>
