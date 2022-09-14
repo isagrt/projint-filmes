640a09eb9c270fbf47e23270e2b2594897d36bbf
isagrt <isadoragoedert05@gmail.com>
2022-09-14 09:13:48 -0300

    basico

--- src/components/template/HeaderComp.vue
+++ src/components/template/HeaderComp.vue
@@ -12,7 +12,5 @@ header span {
   padding-right: 2%;
 }
 header {
-  border-bottom: 1px solid black;
-  padding: 1%;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup></script>
 
 <template>
-  <main>oii</main>
+  <main>corpo do trabalho</main>
 </template>
